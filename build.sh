1a6c670021d9f7aeddb2763c2ecd42a832ef421e
Gleb Mazovetskiy <glex.spb@gmail.com>
2024-07-31 23:13:26 +0100

    Check for flac and lame in build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,32 @@ set -euo pipefail
 
 declare -ra LANGS=(es pl ru)
 
+declare -rA DEPENDENCY_SOURCES=(
+  [pcx2clx]=https://github.com/diasurgical/devilutionx-graphics-tools/
+)
+declare -ra DEPENDENCIES=(pcx2clx flac lame)
+
+check_deps() {
+  local -a missing_deps=()
+  local path
+  for dep in "${DEPENDENCIES[@]}"; do
+    if path="$(which "$dep")"; then
+      echo >&2 "Using $dep from $path"
+    else
+      missing_deps+=("$dep")
+    fi
+  done
+  if (( ${#missing_deps[@]} )); then
+    echo >&2 "Error: Missing dependencies"
+    for dep in "${missing_deps[@]}"; do
+      echo >&2 '* '"Please install \"${dep}\" from ${DEPENDENCY_SOURCES[$dep]:-your package manager}"
+    done
+    exit 1
+  fi
+}
+
 main() {
+  check_deps
   set -x
   build_fonts_mpq
   for lang in "${LANGS[@]}"; do
@@ -14,10 +39,6 @@ main() {
 }
 
 build_fonts_mpq() {
-  if ! which pcx2clx; then
-    echo "Please install pcx2clx from https://github.com/diasurgical/devilutionx-graphics-tools/"
-    exit 1
-  fi
   rm -f fonts.mpq
   mkdir -p build/fonts
   cp assets/fonts/VERSION build/fonts/VERSION
